b9fb399672a6a7531c42ea0f0ffe42b4433a92ab
Jenseric Catbagan <jenseric.catbagan@keyrus.com>
2017-07-23 20:35:12 +0200

    Add errorPage in front

--- productview-web/src/main/java/com/catbagan/productview/ProductviewApplication.java
+++ productview-web/src/main/java/com/catbagan/productview/ProductviewApplication.java
@@ -3,15 +3,16 @@ package com.catbagan.productview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 /**
- * main 
+ * 
  * @author Jenseric.Catbagan
  *
  */
+@SpringBootApplication
 public class ProductviewApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductviewApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(ProductviewApplication.class, args);
+    }
+
 }
